9646b22d0be2c078bf050e770a6bc6eb91f8c4fc
Ksenya Lavrinovich <lavrinovich.kg.13@gmail.com>
2015-11-07 14:50:21 +0300

    all initial cahnges

--- DocumentFlow/Properties/AssemblyInfo.cs
+++ DocumentFlow/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// Управление общими сведениями о сборке осуществляется следующим образом
+// Управление общими сведениями о сборке осуществляется с помощью 
 // набора атрибутов. Измените значения этих атрибутов для изменения сведений,
 // связанных с этой сборкой.
 [assembly: AssemblyTitle("DocumentFlow")]
@@ -10,26 +10,26 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DocumentFlow")]
-[assembly: AssemblyCopyright("© , 2015")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Установка значения False в параметре ComVisible делает типы в этой сборке невидимыми
-// для компонентов COM.  Если требуется обратиться к типу в этой сборке через
+// Установка значения false в параметре ComVisible делает типы в этой сборке невидимыми 
+// для компонентов COM. Если требуется обратиться к типу в этой сборке через 
 // COM, задайте атрибуту ComVisible значение true для требуемого типа.
 [assembly: ComVisible(false)]
 
-// Следующий GUID служит для ID библиотеки типов typelib, если этот проект видим для COM
-[assembly: Guid("d5738cb1-9253-4cc8-af7f-4a2e98535d41")]
+// Следующий GUID служит для идентификации библиотеки типов typelib, если этот проект видим для COM
+[assembly: Guid("f1636bcc-3d52-4d00-aa61-07a4f1bb24df")]
 
 // Сведения о версии сборки состоят из указанных ниже четырех значений:
 //
 //      основной номер версии;
-//      Дополнительный номер версии
+//      дополнительный номер версии;
 //      номер сборки;
 //      редакция.
 //
-// Можно задать все значения или принять номер редакции и номер сборки по умолчанию,
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию, 
 // используя "*", как показано ниже:
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
